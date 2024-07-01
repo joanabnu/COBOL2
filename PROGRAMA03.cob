2f4bb19fe0a6e19595ff3a519a27f0c258119038
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2024-07-01 10:31:59 -0300

    Adicionando variavel mascarar

--- PROGRAMA03.cob
+++ PROGRAMA03.cob
@@ -1,7 +1,7 @@
       ******************************************************************
       * Author: Joana Maria luz de
       * Date:01/07/24
-      * Purpose: E
+      * Purpose: Aprendizado com COBOL
       * Tectonics: cobc
       ******************************************************************
          IDENTIFICATION DIVISION.
@@ -13,10 +13,11 @@
          DATA DIVISION.
          WORKING-STORAGE SECTION.
 
-         77 WRK-NOME            PIC X(30) VALUE SPACES.
-         77 WRK-VALOR           PIC 9(03)V99 VALUE ZEROS.
-         77 WRK-VALOR2          PIC 9(03)V99 VALUE ZEROS.
-         77 WRK-RESULTADO       PIC S9(03)V99 VALUE ZEROS.
+         77 WRK-NOME            PIC X(30)     VALUE SPACES.
+         77 WRK-VALOR           PIC 9(05)V99  VALUE ZEROS.
+         77 WRK-VALOR2          PIC 9(05)V99  VALUE ZEROS.
+         77 WRK-RESULTADO       PIC S9(05)V99 VALUE ZEROS.
+         77 WRK-RESULTADO-ED    PIC -ZZZZ9,99  VALUE ZEROS.
 
          PROCEDURE DIVISION.
          0100-RECEBE SECTION.
@@ -27,11 +28,13 @@
               DISPLAY "Digite o seu valor 2 : "
               ACCEPT WRK-VALOR2.
               COMPUTE WRK-RESULTADO = WRK-VALOR - WRK-VALOR2.
+              MOVE WRK-RESULTADO TO WRK-RESULTADO-ED.
          0200-MOSTRA  SECTION.
               DISPLAY "Nome digitado e : " WRK-NOME.
               DISPLAY "VALOR 1 : " WRK-VALOR.
               DISPLAY "VALOR 2  : " WRK-VALOR2.
-              DISPLAY "resultado : " WRK-RESULTADO.
+              DISPLAY "Resultado : " WRK-RESULTADO.
+              DISPLAY "Resultado ED: " WRK-RESULTADO-ED.
          0300-FINALIZAR SECTION.
               STOP RUN.
               END PROGRAM YOUR-PROGRAM-NAME.
