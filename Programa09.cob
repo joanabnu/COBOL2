73408a9a6b2682d339c0cda9d5efb62a1d48184c
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2024-08-05 11:11:14 -0300

    Comando de decisão

---
+++ Programa09.cob
@@ -0,0 +1,37 @@
+      ******************************************************************
+      * Author: Joana Maria luz de sá
+      * Date: 05/08/2024
+      * Purpose: Comando de decisão IF
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       SPECIAL-NAMES.
+            DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOTA1 PIC 9(02)V99 VALUE ZEROS.
+       77 WRK-NOTA2 PIC 9(02)V99 VALUE ZEROS.
+       77 WRK-MEDIA PIC 9(02)V99 VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+         0100-RECEBE SECTION.
+              DISPLAY "Informe a nota 1 : "
+              ACCEPT WRK-NOTA1
+              DISPLAY "Informe a nota 2 : "
+              ACCEPT WRK-NOTA2.
+         0200-PROCESSAR SECTION.
+              COMPUTE WRK-MEDIA = (WRK-NOTA1 + WRK-NOTA2) / 2.
+         0300-MOSTRA SECTION.
+              DISPLAY "Media : " WRK-MEDIA
+              IF WRK-MEDIA GREATER 7
+                  DISPLAY "APROVADO"
+              ELSE
+                  DISPLAY "REPROVADO"
+              END-IF.
+         0400-FINALIZAR SECTION.
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
---
+++ Programa10.cob
@@ -0,0 +1,39 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+         77 WRK-PRODUTO PIC X(10) VALUE SPACES.
+         77 WRK-ESTADO PIC X(02) VALUE SPACES.
+         77 WRK-VALOR PIC 9(08) VALUE ZEROS.
+         77 WRK-FRETE PIC 9(08)V99 VALUE ZEROS.
+         77 WRK-VALORTOTAL PIC 9(09) VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+         0100-RECEBE SECTION.
+
+              DISPLAY "Informe o produto : "
+              ACCEPT WRK-PRODUTO
+              DISPLAY "Informe o estado : "
+              ACCEPT WRK-ESTADO
+              DISPLAY "Informe o valor do produto : "
+              ACCEPT WRK-VALOR.
+         0200-PROCESSA SECTION.
+
+              IF WRK-ESTADO EQUAL "SP"
+                  COMPUTE WRK-FRETE = WRK-VALOR *0,10
+                  COMPUTE WRK-VALORTOTAL = WRK-FRETE + WRK-VALOR
+                  END-IF.
+         0300-MOSTRA SECTION.
+              DISPLAY "Valor total : " WRK-VALORTOTAL.
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
